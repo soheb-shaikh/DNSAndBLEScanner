3f11e9093f5cbd6482a38dccf22df8451dd51b5f
Soheb Shaikh <shaikhsoheb41@gmail.com>
2023-06-26 17:33:33 +0530

    - Fixing binding parameters

--- app/src/main/java/com/shaikhsoheb/dnsandblescanner/adapters/ScanResultAdapter.kt
+++ app/src/main/java/com/shaikhsoheb/dnsandblescanner/adapters/ScanResultAdapter.kt
@@ -18,8 +18,9 @@ class ScanResultAdapter(
     lateinit var bleBinding: BleScanResultItemBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        bleBinding = BleScanResultItemBinding.inflate(LayoutInflater.from(parent.context))
-        return ViewHolder(bleBinding.root, onClickListener)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.ble_scan_result_item, parent)
+        return ViewHolder(view, onClickListener)
     }
 
     override fun getItemCount() = items.size
@@ -34,14 +35,15 @@ class ScanResultAdapter(
         private val onClickListener: ((device: ScanResult) -> Unit)
     ) : RecyclerView.ViewHolder(view) {
 
+        lateinit var binding: BleScanResultItemBinding
+
         @SuppressLint("MissingPermission")
         fun bind(result: ScanResult) {
-            view.findViewById<TextView>(R.id.tv_device_name).text =
-                result.device.name ?: "Unnamed"
-
-            view.findViewById<TextView>(R.id.tv_mac_address).text = result.device.address
-            view.findViewById<TextView>(R.id.tv_signal_strength).text = "${result.rssi} dBm"
-            view.setOnClickListener { onClickListener.invoke(result) }
+            binding = BleScanResultItemBinding.bind(view)
+            binding.tvDeviceName.text = result.device.name ?: "Unnamed"
+            binding.tvMacAddress.text = result.device.address
+            binding.tvSignalStrength.text = "${result.rssi} dBm"
+            binding.root.setOnClickListener { onClickListener.invoke(result) }
         }
     }
 }
