6b26188b39236a1c8ca7f6f308497d5c9beaa338
Soheb Shaikh <shaikhsoheb41@gmail.com>
2023-06-26 16:05:00 +0530

    - Added permissions helper functions for Bluetooth scanning.

--- app/src/main/java/com/shaikhsoheb/dnsandblescanner/common/PermissionsHelper.kt
+++ app/src/main/java/com/shaikhsoheb/dnsandblescanner/common/PermissionsHelper.kt
@@ -1,8 +1,79 @@
 package com.shaikhsoheb.dnsandblescanner.common
 
-class PermissionsHelper {
+import android.Manifest
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
+import android.view.View.OnClickListener
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-    fun requestBluetoothPermissions() {
+object PermissionsHelper {
 
+    private const val RUNTIME_PERMISSION_REQUEST_CODE = 1001
+
+    fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    fun Context.hasRequiredRuntimePermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            hasPermission(Manifest.permission.BLUETOOTH_SCAN) &&
+                    hasPermission(Manifest.permission.BLUETOOTH_CONNECT)
+        } else {
+            hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun AppCompatActivity.requestRelevantRuntimePermissions() {
+        if (hasRequiredRuntimePermissions()) { return }
+        when {
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.S -> {
+                requestLocationPermission()
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                requestBluetoothPermissions()
+            }
+        }
+    }
+
+    private fun AppCompatActivity.requestLocationPermission() {
+        runOnUiThread {
+            AlertDialog.Builder(this)
+                .setTitle("Location permission required")
+                .setMessage("The system requires apps to be granted location access in order to scan for BLE devices.")
+                .setCancelable(false)
+                .setPositiveButton("OK") { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        this@requestLocationPermission,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        RUNTIME_PERMISSION_REQUEST_CODE
+                    )
+                }.show()
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun AppCompatActivity.requestBluetoothPermissions() {
+        runOnUiThread {
+            AlertDialog.Builder(this)
+                .setTitle("Bluetooth permissions required")
+                .setMessage("The system requires apps to be granted Bluetooth access in order to scan for and connect to BLE devices.")
+                .setCancelable(false)
+                .setPositiveButton("OK") { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ),
+                        RUNTIME_PERMISSION_REQUEST_CODE
+                    )
+                }.show()
+        }
     }
 }
--- app/src/main/java/com/shaikhsoheb/dnsandblescanner/view/MainActivity.kt
+++ app/src/main/java/com/shaikhsoheb/dnsandblescanner/view/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.shaikhsoheb.dnsandblescanner.view
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.shaikhsoheb.dnsandblescanner.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode != Activity.RESULT_OK) {
+            promptEnableBluetooth()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+        }
+    }
+
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startForResult.launch(enableBtIntent)
+        }
     }
 }
